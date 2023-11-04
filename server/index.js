7a22136a510137566f05421ef3d2ccd4fff03b77
Satyam Patel <108532808+patelsatyamgit@users.noreply.github.com>
2023-11-04 15:06:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const server=http.createServer(app);
 
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://nirman-sandy.vercel.app",
         methods:["GET","POST"]
     }
 })
